6e0d5ee7675322d3add71c9666d44eb9b6bd6b1f
trito <therese@trito.se>
2019-12-04 21:55:33 +0100

    Dec01 part 1 in Kotlin

---
+++ kotlinAoC/src/dec01p2kotlin.kt
@@ -0,0 +1,11 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val fileName = "textFiles/Dec01.txt"
+    val modules = File(fileName).readLines().map(String::toInt)
+    var totalFuel = 0;
+    modules.forEach{
+        totalFuel += it/3 - 2
+    }
+    println(totalFuel)
+}
